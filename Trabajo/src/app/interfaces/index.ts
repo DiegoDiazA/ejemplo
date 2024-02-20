a6a695c38ab5a0b6d7b725561f202da2834c5f22
DiegoDiazA <139274059+DiegoDiazA@users.noreply.github.com>
2024-02-20 13:31:01 -0600

    avances clase

---
+++ Trabajo/src/app/interfaces/index.ts
@@ -0,0 +1,7 @@
+export interface TopLevel {
+  id_mae?: number;
+  nombre?: string;
+  apodo?:  string;
+  tel?:    string;
+  foto?:   string;
+}
---
+++ Trabajo/src/app/pages/tab2/tab2.page.ts
@@ -0,0 +1,32 @@
+import { Component } from '@angular/core';
+import { ApiService } from 'src/app/services/api.service';
+import { TopLevel } from 'src/app/interfaces'; // Importar la clase TopLevel desde index.ts
+
+@Component({
+  selector: 'app-tab2',
+  templateUrl: 'tab2.page.html',
+  styleUrls: ['tab2.page.scss'],
+})
+export class Tab2Page {
+  id_mae?: number;
+  nombre?: string;
+  apodo?: string;
+  tel?: string;
+
+  constructor(private apiService: ApiService) {}
+
+  enviarDatos() {
+    const datos: TopLevel = {
+      // Utilizar la clase TopLevel para definir la estructura de datos
+      id_mae: this.id_mae,
+      nombre: this.nombre,
+      apodo: this.apodo,
+      tel: this.tel,
+    };
+
+    this.apiService.postDatos(datos).subscribe((resp) => {
+      console.log(resp);
+      // Aquí puedes manejar la respuesta del servidor como desees
+    });
+  }
+}
